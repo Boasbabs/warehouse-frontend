cf87eb7f9ccda892deacf656d4613416c7b6d07e
Adeyemi Simeon <Boasbabs@users.noreply.github.com>
2022-02-21 15:32:17 +0100

    [feat]features: add article logic

---
+++ src/features/articles/AddArticle.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addArticle } from './articlesSlice';
+
+const AddArticle = () => {
+  const [name, setName] = useState('');
+  const [amountInStock, setAmountInStock] = useState('');
+  const dispatch = useDispatch();
+
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleAmountChange = (e) => setAmountInStock(e.target.value);
+
+  const handleSubmit = () => {
+    if (name && amountInStock) {
+      dispatch(
+        addArticle({
+          id: Math.random(),
+          name,
+          amountInStock
+        })
+      );
+
+      setName('');
+      setAmountInStock('');
+    }
+  };
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <input
+          type="number"
+          id="amountInStock"
+          name="amountInStock"
+          value={amountInStock}
+          onChange={handleAmountChange}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default AddArticle;
--- src/features/articles/index.js
+++ src/features/articles/index.js
@@ -1,1 +1,14 @@
-export { default } from './ListArticles';
+import AddArticle from './AddArticle';
+import ListArticles from './ListArticles';
+
+
+const Articles = () => {
+  return (
+    <>
+      <AddArticle />
+      <ListArticles />
+    </>
+  );
+};
+
+export default Articles;
