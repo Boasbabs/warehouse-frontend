d70459f50a0b221763f5f6af54f1179daaad98cd
Adeyemi Simeon <Boasbabs@users.noreply.github.com>
2022-02-21 11:52:30 +0100

    [feat]component: add Layout component

---
+++ src/base-components/Layout/Layout.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import {
+  Box,
+  useColorModeValue,
+  Drawer,
+  DrawerContent,
+  useDisclosure
+} from '@chakra-ui/react';
+
+import SidebarContent from '../SidebarContent';
+import MobileNav from '../MobileNav';
+
+export default function MainArea({ children }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+        <SidebarContent
+          onClose={() => onClose}
+          display={{ base: 'none', md: 'block' }}
+        />
+        <Drawer
+          autoFocus={false}
+          isOpen={isOpen}
+          placement="left"
+          onClose={onClose}
+          returnFocusOnClose={false}
+          onOverlayClick={onClose}
+          size="full">
+          <DrawerContent>
+            <SidebarContent onClose={onClose} />
+          </DrawerContent>
+        </Drawer>
+        {/* mobilenav */}
+        <MobileNav onOpen={onOpen} />
+        <Box ml={{ base: 0, md: 60 }} p="4" pb={{ base: 200, md: 50 }}>
+          {children}
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+MainArea.propTypes = {
+  children: PropTypes.element.isRequired
+};
