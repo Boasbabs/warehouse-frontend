ea98e055c86d0236ab1d82a4147aa6bf5573580a
Adeyemi Simeon <Boasbabs@users.noreply.github.com>
2022-02-21 23:52:33 +0100

    [feat]features: add toast notifications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { Layout } from './base-components';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Layout>
-      <Outlet />
-    </Layout>
+    <>
+      <Layout>
+        <Outlet />
+      </Layout>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3500,
+          style: {
+            borderRadius: '8px',
+            padding: '16px',
+            fontSize: '12px',
+            color: '#171717',
+          }
+        }}
+      />
+    </>
   );
 }
 
