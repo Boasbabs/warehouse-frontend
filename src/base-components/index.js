0a3a10aa4b1812c9b31d1492c1e4c2ebf2ccf89e
Adeyemi Simeon <Boasbabs@users.noreply.github.com>
2022-02-21 11:54:19 +0100

    [feat]base-component: export components

---
+++ src/base-components/index.js
@@ -0,0 +1,4 @@
+export { default as MobileNav } from './MobileNav';
+export { default as SidebarContent } from './SidebarContent';
+export { default as NavItem } from './NavItem';
+export { default as Layout } from './Layout';
